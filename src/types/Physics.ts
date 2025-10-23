5436b7468a375f7873e8b7267ce1aa7c2468f509
Mag1c54 <Mag1c54@yandex.ru>
2025-10-23 18:17:01 +0300

    wtf)

--- src/types/Physics.ts
+++
@@ -1,8 +0,0 @@
-  import * as THREE from 'three';
-  import { RAPIER } from '../utils/physics';
- export default interface PhysicsObject {
-    mesh: THREE.Mesh;
-    sprite: THREE.Sprite;
-    rigidBody: RAPIER.RigidBody;
-  }
- 
--- src/utils/network.ts
+++
@@ -1,6 +0,0 @@
-import { myPlayerId } from '../../server/src/network';
-
-export function getMyPlayerState(gameState: any[]) {
-    if (!myPlayerId) return null;
-    return gameState.find(p => p.id === myPlayerId);
-}
